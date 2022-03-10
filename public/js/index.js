9ac774fd63837bfbbd2f35a24849ca642fda0ab2
Marius <marius@example.com>
2022-03-10 16:55:49 +0200

    generating posts

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,34 @@ async function getArticles() {
     console.log('yra klaidu redirect');
     window.location.replace('login.html');
   }
-  // renderArticles(jsData.data, PostDestination);
+  renderArticles(jsData.data);
 }
 getArticles();
+
+/* <article class="post">
+  <h2>title</h2>
+  <p>body</p>
+  <p class="footer">data</p>
+</article> */
+function makeArticle(post) {
+  const artEl = document.createElement('article');
+  artEl.className = 'post';
+  const h2El = document.createElement('h2');
+  h2El.textContent = post.title;
+  const pEl = document.createElement('p');
+  pEl.textContent = post.content;
+  const pElFooter = document.createElement('p');
+  pElFooter.className = 'footer';
+  pElFooter.textContent = post.date;
+  artEl.append(h2El, pEl, pElFooter);
+  return artEl;
+}
+function renderArticles(postsArray) {
+  const postsEl = document.createElement('div');
+  postsEl.className = 'posts';
+  postsArray.forEach((post) => {
+    const singlePost = makeArticle(post);
+    postsEl.append(singlePost);
+  });
+  document.body.append(postsEl);
+}
